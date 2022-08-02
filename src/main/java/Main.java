9ab87ae43d273057746354dc268ff17519d93ca6
R30Radium <R30Radium@gmail.ru>
2022-08-02 14:58:16 +0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    {
+        System.out.println("array");
+    }
+
+    public static void quickSort(int[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                swapElements(arr, i, j);
+            }
+        }
+
+        swapElements(arr, i + 1, end);
+        return i + 1;
+    }
+
+    private static void swapElements(int[] arr, int i, int j) {
+    }
+}
